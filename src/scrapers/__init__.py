2f94fc12af39f1315accbace1ae23a71ddd85f26
mzmvyp <willian.prado@ymail.com>
2025-11-28 23:20:23 -0300

    Sync: src/scrapers/__init__.py

---
+++ src/scrapers/__init__.py
@@ -0,0 +1,15 @@
+from .amazon import AmazonScraper
+from .kabum import KabumScraper
+from .mercadolivre import MercadoLivreScraper
+from .royalcaribbean import RoyalCaribbeanScraper
+from .inpower import InpowerScraper
+
+__all__ = [
+    "AmazonScraper",
+    "KabumScraper",
+    "MercadoLivreScraper",
+    "RoyalCaribbeanScraper",
+    "InpowerScraper",
+]
+
+
